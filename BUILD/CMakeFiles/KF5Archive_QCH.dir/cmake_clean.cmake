c39e8464544f5dfb398d0a2daf656c80481c3cf5
Gustavo Castro <gustawho@openmailbox.org>
2017-11-11 01:08:30 -0600

    Directory cleanup

--- BUILD/CMakeFiles/KF5Archive_QCH.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "klook_autogen"
-  "CMakeFiles/klook_autogen.dir/AutogenOldSettings.cmake"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/KF5Archive_QCH.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- BUILD/icons/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gustawho/Projects/klook/icons
-# Build directory: /home/gustawho/Projects/klook/BUILD/icons
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
